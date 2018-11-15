ebb9abf64d3bebb828cf8bce48b077ff6d253ff7
Cristian <cefunck@miuandes.cl>
2018-11-15 19:41:17 -0300

    add Output.csv incorrupto

--- venv/Scripts/process_input.py
+++ venv/Scripts/process_input.py
@@ -1,58 +1,46 @@
 import csv
 
-
 lista = []
 ids = []
 ids_por_borrar = []
+lista_limpia_oficial = []
 
-with open(r'C:\Users\MaríaJosé\PycharmProjects\ProyectoIA\AI-Project\input\Input.csv') as csv_file:
+with open(r'C:\Users\Cristian\PycharmProjects\AI-Project\input\Input.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
         lista.append(row[0].split(';'))
 
-
-
 for l in lista:
-
     if l[1] not in ids:
         ids.append(l[1])
 
-
 for i in ids:
     borrar = True
-    especialidades = []
     for j in lista:
-
         if j[1] == i:
-            especialidades.append(j[2])
             if j[2] != 'ING':
                 borrar = False
                 break
     if borrar == True:
         ids_por_borrar.append(i)
 
-
 for l in lista:
     if l[1] in ids_por_borrar:
         lista.remove(l)
+lista = lista[1:]
 
 def uniq(lst):
-    last = object()
-    for item in lst:
-        if item == last:
-            continue
-        yield item
-        last = item
+    print(len(lst), "largo de la lista con duplicados")
+    return list(set(lst))
 
-def sort_and_deduplicate(l):
-    return list(uniq(sorted(l, reverse=True)))
+for l in lista:
+    linea = ";".join(l)+'\r\n'
+    lista_limpia_oficial.append(linea)
 
-lista_limpia = sort_and_deduplicate(lista)
+lista_limpia_oficial = uniq(lista_limpia_oficial)
+print(len(lista_limpia_oficial), "largo de la lista sin duplicados")
 
-for l in lista_limpia:
-    print(l)
-
-#with open(r'C:\Users\MaríaJosé\PycharmProjects\ProyectoIA\AI-Project\input\Output.csv', 'w') as myfile:
- #   wr = csv.writer(myfile, lineterminator= '\n')
-  #  wr.writerow(lista_limpia)
+file = open(r'C:\Users\Cristian\PycharmProjects\AI-Project\input\Output.csv','w')
+for i in lista_limpia_oficial:
+    file.write(i)
