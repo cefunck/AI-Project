b60680dab55765286c316bfc4ca595c3584fcf4c
Cristian <cefunck@miuandes.cl>
2018-11-16 15:29:43 -0300

    process input whit error but is in the way

--- venv/Scripts/process_input.py
+++ venv/Scripts/process_input.py
@@ -1,15 +1,19 @@
-import csv
-
 lista = []
 ids = []
 ids_por_borrar = []
 lista_limpia_oficial = []
+id_oficiales = []
+semestres = []
+ramos = []
+matriz = [[]]
+id_icc = []
+id_ici = []
+id_ico = []
+id_ice = []
 
-with open(r'C:\Users\Cristian\PycharmProjects\AI-Project\input\Input.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        lista.append(row[0].split(';'))
+file = open(r'C:\Users\Cristian\PycharmProjects\AI-Project\input\Input.csv', 'r')
+for i in file.readlines():
+    lista.append(i.split(';'))
 
 for l in lista:
     if l[1] not in ids:
@@ -18,10 +22,9 @@ for l in lista:
 for i in ids:
     borrar = True
     for j in lista:
-        if j[1] == i:
-            if j[2] != 'ING':
-                borrar = False
-                break
+        if j[1] == i and j[2] != 'ING':
+            borrar = False
+            break
     if borrar == True:
         ids_por_borrar.append(i)
 
@@ -30,16 +33,63 @@ for l in lista:
         lista.remove(l)
 lista = lista[1:]
 
+
+for i in lista:
+    id_oficiales.append(i[1])
+    semestres.append((i[0]))
+    ramos.append(i[4])
+id_oficiales = list(set(id_oficiales))
+semestres = list(set(semestres))
+ramos = list(set(ramos))
+
+for id in id_oficiales:
+    info = []
+    for l in lista:
+        if l[1] == id:
+            info.append(l)
+    definitiva = []
+    for inf in info:
+        if definitiva == []:
+            definitia = inf
+        elif (int(definitiva[0]) < int(inf[0])):
+            defitinita = inf
+    if definitiva[2] == "INGC":
+        id_icc.append(definitiva[1])
+    elif definitiva[2] == "INGE":
+        id_ice.append(definitiva[1])
+    elif definitiva[2] == "INGO":
+        id_ico.append(definitiva[1])
+    elif definitiva[2] == "INGI":
+        id_ici.append(definitiva[1])
+
+print("icc: ",len(id_icc)," ice: ",len(id_ice)," ico: ",len(id_ico)," ici: ",len(id_ici))
+
+
+for i in semestres:
+    for j in ramos:
+        matriz[-1].append(j)
+
+for id in id_oficiales:
+    matriz.append([])
+    for s in semestres:
+        for r in ramos:
+            for line in lista:
+                if line[0] == s:
+                    if line[1] == id:
+                        if line[4] == r:
+                            matriz[-1].append(line[5][:-1])
+                            print(line)
+                        else:
+                            matriz[-1].append(0)
+
 def uniq(lst):
-    print(len(lst), "largo de la lista con duplicados")
     return list(set(lst))
 
-for l in lista:
+for l in matriz:
     linea = ";".join(l)+'\r\n'
     lista_limpia_oficial.append(linea)
 
 lista_limpia_oficial = uniq(lista_limpia_oficial)
-print(len(lista_limpia_oficial), "largo de la lista sin duplicados")
 
 file = open(r'C:\Users\Cristian\PycharmProjects\AI-Project\input\Output.csv','w')
 for i in lista_limpia_oficial:
