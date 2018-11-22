f63b87247d8218dc4b8812c1bc9778647ad48753
Maria Jose Gonzalez <mjgonzalez6@miuandes.cl>
2018-11-22 06:36:17 -0300

    Update normalizeInput.py

--- venv/Scripts/normalizeInput.py
+++ venv/Scripts/normalizeInput.py
@@ -1,13 +1,14 @@
 def notaNormalizada(strNota):
     if(strNota == str(0)):
         return "-1"
+    print(strNota)
     n = ((float(strNota)-1)/6.0)
     return str(n)
 
 csv = []
 csvOficial = []
 csvOficial2 = []
-file = open(r'C:\Users\paulo\PycharmProjects\AI-Project\input\OutputOficial.csv', 'r')
+file = open(r'C:\Users\MaríaJosé\PycharmProjects\ProyectoIA\AI-Project\input\Output2vSB.csv', 'r')
 for i in file.readlines():
     csv.append(i.split(';'))
 
@@ -20,7 +21,7 @@ for l in csvOficial:
     linea = ";".join(l)+'\r\n'
     csvOficial2.append(linea)
 
-file = open(r'C:\Users\paulo\PycharmProjects\AI-Project\input\Normalizado.csv','w')
+file = open(r'C:\Users\MaríaJosé\PycharmProjects\ProyectoIA\AI-Project\input\OFICIAL.csv','w')
 for i in csvOficial2:
     file.write(i)
 
